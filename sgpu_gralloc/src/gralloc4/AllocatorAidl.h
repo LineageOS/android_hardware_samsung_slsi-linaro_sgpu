fca7e35f9329d6fd0856fa0979597ebee2e7f10e
borbelyvince <borbely.vince.11@gmail.com>
2025-10-19 13:18:14 +0200

    sgpu_gralloc: gralloc4: Convert gralloc4 service to AIDL
    Change-Id: I64d5328040892d83ee0abda20adec52e98e2e53f

---
+++ sgpu_gralloc/src/gralloc4/AllocatorAidl.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <aidl/android/hardware/graphics/allocator/BnAllocator.h>
+#include "core/allocator.h"
+
+using aidl::android::hardware::common::NativeHandle;
+
+namespace aidl::android::hardware::graphics::allocator {
+
+using aidl::android::hardware::graphics::allocator::AllocationResult;
+using aidl::android::hardware::graphics::allocator::BufferDescriptorInfo;
+
+class Allocator : public BnAllocator {
+public:
+    Allocator() {};
+    ndk::ScopedAStatus allocate(const std::vector<uint8_t>& in_descriptor, int32_t count, AllocationResult *_aidl_return);
+    ndk::ScopedAStatus allocate2(const BufferDescriptorInfo& descriptor, int32_t count, AllocationResult *_aidl_return);
+    ndk::ScopedAStatus isSupported(const BufferDescriptorInfo& descriptor, bool *_aidl_return);
+    ndk::ScopedAStatus getIMapperLibrarySuffix(std::string *_aidl_return);
+private:
+    ::android::samsung::gralloc::Allocator mAllocator;
+};
+
+}
