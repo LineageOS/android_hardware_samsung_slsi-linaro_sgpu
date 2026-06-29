e12a9a53574b3e571cc9322b63aaa23eb2d93069
Francescodario Cuzzocrea <bosconovic@gmail.com>
2026-06-30 00:19:47 +0200

    sgpu_gralloc: gralloc4: handle errors in aidl service
    Change-Id: Ifa1f85449ccd9544729d877b2775829ba58e5692
    Signed-off-by: Francescodario Cuzzocrea <bosconovic@gmail.com>

--- sgpu_gralloc/src/gralloc4/AllocatorAidl.cpp
+++ sgpu_gralloc/src/gralloc4/AllocatorAidl.cpp
@@ -6,6 +6,7 @@
 
 #define LOG_TAG "android.hardware.graphics.allocator-aidl-service-sgr"
 
+#include <aidl/android/hardware/graphics/allocator/AllocationError.h>
 #include "AllocatorAidl.h"
 #include <aidlcommonsupport/NativeHandle.h>
 #include "converter.h"
@@ -26,17 +27,32 @@ ndk::ScopedAStatus Allocator::allocate(const std::vector<uint8_t>& in_descriptor
             reinterpret_cast<const BufferDescriptor*>(in_descriptor.data());
     error = mAllocator.allocate(*buffer_descriptor, count, handles, &stride);
 
-    if (error == Error::NONE) {
-        _aidl_return->buffers.resize(handles.size());
-        for (int i = 0; i < handles.size() ; i++) {
-            _aidl_return->buffers[i] = dupToAidl(handles[i]);
-        }
-    } else {
-        _aidl_return->buffers.resize(0);
-        handles.resize(0);
+    switch (error) {
+        case Error::NONE:
+            break;
+
+        case Error::BAD_DESCRIPTOR:
+            return ndk::ScopedAStatus::fromServiceSpecificError(
+                static_cast<int32_t>(AllocationError::BAD_DESCRIPTOR));
+
+        case Error::NO_RESOURCES:
+            return ndk::ScopedAStatus::fromServiceSpecificError(
+                static_cast<int32_t>(AllocationError::NO_RESOURCES));
+
+        case Error::UNSUPPORTED:
+            return ndk::ScopedAStatus::fromServiceSpecificError(
+                static_cast<int32_t>(AllocationError::UNSUPPORTED));
+
+        default:
+            return ndk::ScopedAStatus::fromStatus(STATUS_UNKNOWN_ERROR);
     }
 
+    _aidl_return->buffers.resize(handles.size());
+
     _aidl_return->stride = static_cast<int>(stride);
+    for (int i = 0; i < handles.size() ; i++) {
+        _aidl_return->buffers[i] = dupToAidl(handles[i]);
+    }
 
     for (auto handle : handles) {
         mAllocator.free_handle(handle);
